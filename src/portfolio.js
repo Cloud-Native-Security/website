85684d191d7ff69c3c001b5153615419616d4b1a
Alistair Hey <alistair@heyal.co.uk>
2023-01-31 15:22:22 +0000

    Fix link for homepage
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'devsecopstutorials.com',
+  homepage: 'https://devsecopstutorials.com/',
   title: 'Cloud Native Security.',
 }
 
