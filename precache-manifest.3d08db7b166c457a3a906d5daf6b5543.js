0ec806e574440747fa86ea85a8b54149ce20adcc
AnaisUrlichs <AnaisUrlichs@users.noreply.github.com>
2023-06-27 08:05:07 +0000

    deploy: 482bae528d075ae3d6e86bc7ae1f9779d47a1de8

---
+++ precache-manifest.3d08db7b166c457a3a906d5daf6b5543.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e852ad7aaaa585534173",
+    "url": "./static/css/main.6937b68f.chunk.css"
+  },
+  {
+    "revision": "e852ad7aaaa585534173",
+    "url": "./static/js/main.1cd5832d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "08625c1aa842c1c187db",
+    "url": "./static/js/2.18898256.chunk.js"
+  },
+  {
+    "revision": "fcbceb39dce31c8c198be9a3a1f178f6",
+    "url": "./index.html"
+  }
+];
